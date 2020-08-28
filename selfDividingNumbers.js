4a512621ea16db3bf200060592720b83b8bba059
hector-felix <62691263+hector-felix@users.noreply.github.com>
2020-08-27 17:40:19 -0700

    Add files via upload

---
+++ selfDividingNumbers.js
@@ -0,0 +1,20 @@
+var selfDividingNumbers = function(left, right) {
+    let result = [];
+    for(let i = left; i <= right; i++) {
+        let num = i;
+				let selfDiv = true;
+        while(num > 0) {
+					let digit = num%10;
+					num = parseInt(num/10);
+          if(i % digit !== 0) {
+                selfDiv = false;
+                break;
+            }
+				}
+				if(selfDiv)
+      		result.push(i);
+    }
+		return result;
+};
+
+console.log(selfDividingNumbers(1,1000));
