d2d32e2e201e8a0cf7f27e302f65da74af78382e
hector-felix <62691263+hector-felix@users.noreply.github.com>
2020-08-28 11:29:29 -0700

    Update selfDividingNumbers.js

--- selfDividingNumbers.js
+++ selfDividingNumbers.js
@@ -17,22 +17,22 @@ The boundaries of each input argument are 1 <= left <= right <= 10000.
 */
 
 var selfDividingNumbers = function(left, right) {
-    let result = [];
-    for(let i = left; i <= right; i++) {
-        let num = i;
-				let selfDiv = true;
-        while(num > 0) {
-					let digit = num%10;
-					num = parseInt(num/10);
-          if(i % digit !== 0) {
-                selfDiv = false;
-                break;
-            }
-				}
-				if(selfDiv)
-      		result.push(i);
-    }
-		return result;
+	let result = [];
+	for(let i = left; i <= right; i++) {
+		let num = i;
+		let selfDiv = true;
+		while(num > 0) {
+			let digit = num%10;
+			num = parseInt(num/10);
+			if(i % digit !== 0) {
+				selfDiv = false;
+				break;
+			}
+		}
+		if(selfDiv)
+			result.push(i);
+	}
+	return result;
 };
 
 console.log(selfDividingNumbers(1,1000));
